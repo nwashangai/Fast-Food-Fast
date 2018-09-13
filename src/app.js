9bfeb11657a4d229f7c3124da9471fc0963a5db8
nwashangai <nwashangai@users.noreply.github.com>
2018-09-13 18:25:00 +0100

    #160480792 setup server (#7)
    * ch- server setup
        - install and setup express
        - setup API structure
        - setup test module
        [Finishes #160480792]

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import config from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import router from './routes/';
+
+const app = express();
+require('dotenv').config();
+config.config();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/v1', router);
+app.use((req, res, next) => {
+  res.status(404);
+  res.send({ error: '404 Sorry the page has not yet been defined try /api/v1/' });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server started at port 3000');
+});
+
+module.exports = app; // for testing
