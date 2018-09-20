fb1f700a2a2d034669579e3f1aa800a14f0e3e26
nwashangai <nwashangai@users.noreply.github.com>
2018-09-20 11:11:32 +0100

    #160629658 link pages (#21)
    * bg- link pages
        - link login form to user page
        - liink signup form to admin page
        [Finishes #160629658]
    
    * #160638518 add user cat (#23)
    
    * ch- add user cat
       - add cat section
      -  dynamically add items
    Finishes[#160638518]
    
    * bg- fix edit (#24)
    
    - add ability to edit food item
        [Finishes #160643686]

--- UI/assets/js/form.js
+++ UI/assets/js/form.js
@@ -1,14 +1,21 @@
 const checkForm = () => {
-    if (!document.getElementById('signup').checkValidity()) {
-        // $('form').find('input[type="submit"]').click();
-        alert('nooo');
+    if (!document.getElementById('login-form').checkValidity()) {
+        alert('please provide correct input values')
         return false;
     }
+    window.location.replace("user.html");
+}
+
+const checkSignUpForm = () => {
+    if (!document.getElementById('signup').checkValidity()) {
+        alert('please provide correct input values')
+        return false;
+    }
+    window.location.replace("admin.html");
 }
 
 const checkOrderForm = () => {
     if (!document.getElementById('food-data').checkValidity()) {
-        alert('nooo');
         return false;
     }
 }
